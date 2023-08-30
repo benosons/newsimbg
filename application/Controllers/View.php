e6d531c346672419818b78f3324b1be806c1d83c
fachriagustian12 <fachri.agustian12@gmail.com>
2023-08-30 14:24:41 +0700

    add new file-menu-tiap-role and modify access-role-in-view-controller

--- application/Controllers/View.php
+++ application/Controllers/View.php
@@ -56,7 +56,7 @@ class View extends \CodeIgniter\Controller
 
 	public function login()
 	{
-		
+
 		if ($this->logged) {
 			return redirect('dashboard');
 		} else {
@@ -74,7 +74,7 @@ class View extends \CodeIgniter\Controller
 
 	public function register()
 	{
-		
+
 		if ($this->logged) {
 			return redirect('dashboard');
 		} else {
@@ -97,7 +97,7 @@ class View extends \CodeIgniter\Controller
 
 	// 	return \Twig::instance()->display('front/layanan_informasi/mahasiswa.html',$this->data);
 	// }
-	
+
 	// public function layanan_informasi_dosen()
 	// {
 	// 	helper('url');
@@ -165,15 +165,15 @@ class View extends \CodeIgniter\Controller
 	public function permohonan()
 	{
 
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 10)) {
 			helper('form');
 			$perm = new \App\Models\KegiatanModel();
-		  	
+
 			$this->data['active'] = 'permohonan';
 			$this->data['data_jenis']		= $perm->getjenis();
 			$this->data['data_fungsi']		= $perm->getfungsi();
 			$this->data['data_jbg']		= $perm->getjbg();
-			
+
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/permohonan/permohonan.js';
 			return \Twig::instance()->display('admin/permohonan/permohonan.html', $this->data);
 		} else {
@@ -184,7 +184,7 @@ class View extends \CodeIgniter\Controller
 	public function verifikasi_dokumen()
 	{
 
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 20)) {
 			helper('form');
 			$this->data['active'] = 'verifikasi_dokumen';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/dinas_teknis/verifikasi_dokumen.js';
@@ -197,7 +197,7 @@ class View extends \CodeIgniter\Controller
 	public function bangunan_bertahap()
 	{
 
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 20)) {
 			helper('form');
 			$this->data['active'] = 'bangunan_bertahap';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/dinas_teknis/bangunan_bertahap.js';
@@ -210,7 +210,7 @@ class View extends \CodeIgniter\Controller
 	public function profil_pu()
 	{
 
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 20 || $this->data['role'] == 30 || $this->data['role'] == 40 || $this->data['role'] == 50 || $this->data['role'] == 60 || $this->data['role'] == 70)) {
 			helper('form');
 			$this->data['active'] = 'profil_pu';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/pengaturan/profil_pu.js';
@@ -223,7 +223,7 @@ class View extends \CodeIgniter\Controller
 	public function data_asn()
 	{
 
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 20)) {
 			helper('form');
 			$this->data['active'] = 'data_asn';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/pengaturan/data_asn.js';
@@ -236,7 +236,7 @@ class View extends \CodeIgniter\Controller
 	public function input_tpa()
 	{
 
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 20 || $this->data['role'] == 30 || $this->data['role'] == 40)) {
 			helper('form');
 			$this->data['active'] = 'input_tpa';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/pengaturan/input_tpa.js';
@@ -249,7 +249,7 @@ class View extends \CodeIgniter\Controller
 	public function surat_keputusan()
 	{
 
-		if ($this->logged) {
+		if ($this->logged  && ($this->data['role'] == 20 || $this->data['role'] == 30 || $this->data['role'] == 40)) {
 			helper('form');
 			$this->data['active'] = 'surat_keputusan';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/pengaturan/surat_keputusan.js';
@@ -262,7 +262,7 @@ class View extends \CodeIgniter\Controller
 	public function data_kampus()
 	{
 
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 20)) {
 			helper('form');
 			$this->data['active'] = 'data_kampus';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/pengaturan/data_kampus.js';
@@ -275,7 +275,7 @@ class View extends \CodeIgniter\Controller
 	public function konsultasi()
 	{
 
-		if ($this->logged) {
+		if ($this->logged  && ($this->data['role'] == 20 || $this->data['role'] == 30 || $this->data['role'] == 50 || $this->data['role'] == 60)) {
 			helper('form');
 			$this->data['active'] = 'konsultasi';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/monitoring/konsultasi.js';
@@ -288,7 +288,7 @@ class View extends \CodeIgniter\Controller
 	public function pengawas_pupr()
 	{
 
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 40)) {
 			helper('form');
 			$this->data['active'] = 'pengawas_pupr';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/pengawas_teknis/pengawas_pupr.js';
@@ -301,7 +301,7 @@ class View extends \CodeIgniter\Controller
 	public function penilaian_konsultasi()
 	{
 
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 30)) {
 			helper('form');
 			$this->data['active'] = 'penilaian_konsultasi';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/pengawas_teknis/penilaian_konsultasi.js';
@@ -313,7 +313,7 @@ class View extends \CodeIgniter\Controller
 
 	public function inspeksi()
 	{
-		if ($this->logged) {
+		if ($this->logged  && ($this->data['role'] == 20 || $this->data['role'] == 30 || $this->data['role'] == 40)) {
 			helper('form');
 			$this->data['active'] = 'inspeksi';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/pengawas_teknis/inspeksi.js';
@@ -325,7 +325,7 @@ class View extends \CodeIgniter\Controller
 
 	public function validasi_rekomtek_kadis()
 	{
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 40)) {
 			helper('form');
 			$this->data['active'] = 'validasi_rekomtek_kadis';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/validasi_rekom/validasi_rekomtek_kadis.js';
@@ -337,7 +337,7 @@ class View extends \CodeIgniter\Controller
 
 	public function validasi_hasil_inspeksi()
 	{
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 40)) {
 			helper('form');
 			$this->data['active'] = 'validasi_hasil_inspeksi';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/validasi_hasil_inspeksi/validasi_hasil_inspeksi.js';
@@ -349,7 +349,7 @@ class View extends \CodeIgniter\Controller
 
 	public function penagihan_retribusi()
 	{
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 50)) {
 			helper('form');
 			$this->data['active'] = 'penagihan_retribusi';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/penagihan_retribusi/penagihan_retribusi.js';
@@ -361,7 +361,7 @@ class View extends \CodeIgniter\Controller
 
 	public function penyerahan_dokumen()
 	{
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 50)) {
 			helper('form');
 			$this->data['active'] = 'penyerahan_dokumen';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/penyerahan_dokumen/penyerahan_dokumen.js';
@@ -373,7 +373,7 @@ class View extends \CodeIgniter\Controller
 
 	public function penerbitan_dokumen()
 	{
-		if ($this->logged) {
+		if ($this->logged && ($this->data['role'] == 70)) {
 			helper('form');
 			$this->data['active'] = 'penerbitan_dokumen';
 			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/penerbitan_dokumen/penerbitan_dokumen.js';
@@ -459,10 +459,10 @@ class View extends \CodeIgniter\Controller
 		if ($this->logged) {
 			helper('form');
 			$user = new \App\Models\UserModel();
-		  	
+
 			$this->data['active'] 			= 'users';
 			$this->data['data_role']		= $user->getrole();
-			
+
 			$this->data['script'] 			= $this->data['baseURL'] . '/action-js/admin/user/user.js';
 			return \Twig::instance()->display('admin/user/index.html', $this->data);
 		} else {
@@ -476,17 +476,17 @@ class View extends \CodeIgniter\Controller
 		if ($this->logged) {
 			helper('form');
 			$user = new \App\Models\UserModel();
-		  	
+
 			$this->data['active'] 			= 'vouchers';
 			$this->data['data_role']		= $user->getrole();
-			
+
 			$this->data['script'] 			= $this->data['baseURL'] . '/action-js/admin/vouchers.js';
 			return \Twig::instance()->display('admin/vouchers.html', $this->data);
 		} else {
 			return redirect('dashboard');
 		}
 	}
-	
+
 	public function dealerData()
 	{
 		if ($this->logged) {
@@ -495,7 +495,7 @@ class View extends \CodeIgniter\Controller
 
 			$this->data['active'] = 'dealerData';
 			$this->data['data_role']		= $user->getrole();
-			$this->data['script'] = $this->data['baseURL'].'/action-js/admin/dealerData/dealerData.js';
+			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/dealerData/dealerData.js';
 			return \Twig::instance()->display('admin/dealerData/dealerData.html', $this->data);
 		} else {
 			return redirect('dashboard');
@@ -510,23 +510,23 @@ class View extends \CodeIgniter\Controller
 
 			$this->data['active'] = 'payment';
 			$this->data['data_role']		= $user->getrole();
-			$this->data['script'] = $this->data['baseURL'].'/action-js/admin/payment.js';
+			$this->data['script'] = $this->data['baseURL'] . '/action-js/admin/payment.js';
 			return \Twig::instance()->display('admin/payment/payment.html', $this->data);
 		} else {
 			return redirect('dashboard');
 		}
 	}
-	
+
 	public function reportVouchers()
 	{
 
 		if ($this->logged) {
 			helper('form');
 			$user = new \App\Models\UserModel();
-		  	
+
 			$this->data['active'] 			= 'vouchers';
 			$this->data['data_role']		= $user->getrole();
-			
+
 			$this->data['script'] 			= $this->data['baseURL'] . '/action-js/admin/reportVouchers.js';
 			return \Twig::instance()->display('admin/reportVouchers.html', $this->data);
 		} else {
@@ -540,10 +540,10 @@ class View extends \CodeIgniter\Controller
 		if ($this->logged) {
 			helper('form');
 			$user = new \App\Models\UserModel();
-		  	
+
 			$this->data['active'] 			= 'token';
 			$this->data['data_role']		= $user->getrole();
-			
+
 			$this->data['script'] 			= $this->data['baseURL'] . '/action-js/admin/tokenData.js';
 			return \Twig::instance()->display('admin/tokenData/tokenData.html', $this->data);
 		} else {
@@ -556,7 +556,7 @@ class View extends \CodeIgniter\Controller
 
 		if ($this->logged) {
 			helper('form');
-		  	
+
 			$this->data['here'] 			= 'log';
 			$this->data['script'] 			= $this->data['baseURL'] . '/action-js/admin/user/log.js';
 			return \Twig::instance()->display('admin/user/log.html', $this->data);
@@ -570,10 +570,10 @@ class View extends \CodeIgniter\Controller
 
 	// 	if ($this->logged) {
 	// 		helper('form');
-			
+
 	// 		$request	= $this->request;
 	// 		$detail 	= $request->getVar('detail');
-			
+
 	// 		$this->data['here'] 			= 'infobox';
 	// 		$this->data['box'] 				= $detail;
 	// 		$this->data['script'] 			= $this->data['baseURL'] . '/action-js/admin/infobox.js';
@@ -591,15 +591,15 @@ class View extends \CodeIgniter\Controller
 	// 		$request	= $this->request;
 	// 		$id_tiket 	= $request->getVar('detail');
 	// 		$komunikasi = new \App\Models\KomunikasiModel();
-			
+
 	// 		if($id_tiket){
 	// 			$this->data['detail_tiket'] 	= $komunikasi->gettiket(null, $this->session->get('role'), $id_tiket);
 	// 			$this->data['diskusi_tiket'] 	= $komunikasi->getdiskusitiket($id_tiket);
-				
+
 	// 		}else{
 	// 			$this->data['data_tiket'] 		= $komunikasi->gettiket($this->session->get('user_id'), $this->session->get('role'));
 	// 		}
-			
+
 	// 		$this->data['here'] 			= 'komunikasi';
 	// 		$this->data['script'] 			= $this->data['baseURL'] . '/action-js/admin/komunikasi.js';
 	// 		return \Twig::instance()->display('admin/komunikasi.html', $this->data);
@@ -642,11 +642,11 @@ class View extends \CodeIgniter\Controller
 	// 			$this->data['data_denda'] 	= $cicilan->getdetailcicilandenda($request->uri->getSegment(2));
 	// 			$this->data['data_billing'] = $billing->getbilling($request->uri->getSegment(2));
 	// 		}else if($request->uri->getSegment(1) == 'detailbilling'){
-				
+
 	// 			$this->data['detail_billing'] = $billing->getdetailbilling($request->uri->getSegment(2));
 	// 			$this->data['detail_pembayaran_billing'] = $billing->getdetailpembayaranbilling($request->uri->getSegment(2));
-				
-				
+
+
 	// 		}else{
 	// 			$this->data['data_provinsi'] 	= $user->getprovinsi();
 	// 			$this->data['data_lembaga'] 	= $user->getlembaga();
